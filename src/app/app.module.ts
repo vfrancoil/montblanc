9f04bfcb878e16f2c4c20a426f7a9f55eceed6ef
vfrancoil <vfrancoil@hotmail.fr>
2022-06-05 08:52:23 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './component/nav/nav.component';
+import { InfoComponent } from './component/info/info.component';
+import { MainComponent } from './component/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    InfoComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
